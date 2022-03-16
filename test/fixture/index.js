cd98b88b48b86242e79791287c9e096b44d87328
K <7907829-fa_rfro_mwrong@users.noreply.gitlab.com>
2022-03-16 20:09:39 +0100

    upgrade deps

--- test/fixture/index.js
+++ test/fixture/index.js
@@ -15,64 +15,67 @@ let sio;
 
 exports.start = (options, callback) => {
 
-  if (typeof options == 'function') {
-    callback = options;
-    options = {};
-  }
+	if (typeof options == 'function') {
+		callback = options;
+		options = {};
+	}
 
-  options = xtend({
-    secret: 'aaafoo super sercret',
-    timeout: 1000,
-    handshake: true
-  }, options);
+	options = xtend({
+		secret: 'aaafoo super sercret',
+		timeout: 1000,
+		handshake: true
+	}, options);
 
-  const app = express();
-  const server = http.createServer(app);
-  sio = socketIo.listen(server);
+	const app = express();
+	const server = http.createServer(app);
+	sio = socketIo(server, {
+		allowEIO3: false,
+		pingTimeout: 2000, pingInterval: 3000
+	});
 
-  app.use(bodyParser.json());
-  app.post('/login', (req, res) => {
-    const profile = {
-      first_name: 'John',
-      last_name: 'Doe',
-      email: 'john@doe.com',
-      id: 123
-    };
+	app.use(bodyParser.json());
+	app.post('/login', (req, res) => {
+		const profile = {
+			first_name: 'John',
+			last_name: 'Doe',
+			email: 'john@doe.com',
+			id: 123
+		};
 
-    // We are sending the profile inside the token
-    const token = jwt.sign(profile, options.secret, { expiresIn: 60*60*5 });
-    res.json({token: token});
-  });
+		// We are sending the profile inside the token
+		const token = jwt.sign(profile, options.secret, { expiresIn: 60 * 60 * 5 });
+		res.json({ token: token });
+	});
 
 
-  if (options.handshake) {
-    sio.use(socketio_jwt.authorize(options));
+	if (options.handshake) {
+		sio.use(socketio_jwt.authorize(options));
 
-    sio.sockets.on('echo', (m) => {
-      sio.sockets.emit('echo-response', m);
-    });
-  } else {
-    sio.sockets
-      .on('connection', socketio_jwt.authorize(options))
-      .on('authenticated', (socket) => {
-        socket.on('echo', (m) => {
-          socket.emit('echo-response', m);
-        });
-      });
-  }
+		sio.sockets.on('echo', (m) => {
+			sio.sockets.emit('echo-response', m);
+		});
+	} else {
+		sio.sockets
+			.on('connection', socketio_jwt.authorize(options))
+			.on('authenticated', (socket) => {
+				socket.on('echo', (m) => {
+					socket.emit('echo-response', m);
+				});
+			});
+	}
 
-  server.__sockets = [];
-  server.on('connection', (c) => {
-    server.__sockets.push(c);
-  });
-  server.listen(9000, callback);
-  enableDestroy(server);
+	server.__sockets = [];
+	server.on('connection', (c) => {
+		server.__sockets.push(c);
+	});
+	server.listen(9000, callback);
+	enableDestroy(server);
 };
 
 exports.stop = (callback) => {
-  sio.close();
-  try {
-    server.destroy();
-  } catch (er) {}
-  callback();
+	sio.close();
+	try {
+		server.destroy();
+	} catch (er) { }
+	callback();
 };
